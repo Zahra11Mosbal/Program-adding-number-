d5fcb8d471b09630959054ea3da71c8c1e6b051f
Zahra11Mosbal <107762291+Zahra11Mosbal@users.noreply.github.com>
2022-12-04 20:05:14 +0200

    linked list
    I dded new method that macke user can collecting number how maney he want.

---
+++ add number/main.java
@@ -0,0 +1,29 @@
+
+
+
+
+
+import java.util.Scanner;
+
+
+public class Main
+{
+    public static void main(String[] args) {
+        
+        System.out.println("enter how many namber you whant to summing : ");
+    
+        Scanner in = new Scanner(System.in);
+        Double i =in.nextDouble();
+        Double j;
+        for(j=0,j<=i,j++){
+          
+        System.out.println("enter number : ");
+           
+         Double num1 =in.nextDudle();
+           
+           LinkedList.insert(num1);
+    } 
+          LinkedList.sum() ;   
+    }
+
+ }
